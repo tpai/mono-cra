9958c21f61e5fdfdb3e727192dd3e755eb6b114d
Tony Pai <tony77794@gmail.com>
2019-03-08 14:53:44 +0800

    Remove ESLint config override

--- packages/app/config-overrides.js
+++ packages/app/config-overrides.js
@@ -1,11 +1,10 @@
 const path = require('path');
 
-module.exports = function configOverrides(config, env) {
-  config.resolve.alias = Object.assign({}, config.resolve.alias, {
-    'assets': path.resolve(__dirname, './src/assets'),
-  });
-  // remove eslint check from create-react-app
-  const moduleRules = config.module.rules;
-  config.module.rules = moduleRules.slice(0, 1).concat(moduleRules.slice(2, moduleRules.length));
-  return config;
+module.exports = {
+  webpack: (config, env) => {
+    config.resolve.alias = Object.assign({}, config.resolve.alias, {
+      'assets': path.resolve(__dirname, './src/assets'),
+    });
+    return config;
+  },
 };
