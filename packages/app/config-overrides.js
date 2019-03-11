0c2ef7b9d3dce98c723221f5a9dee4a633b63baf
Tony Pai <tony77794@gmail.com>
2019-03-11 12:24:28 +0800

    Setup jest configs

--- packages/app/config-overrides.js
+++ packages/app/config-overrides.js
@@ -4,7 +4,19 @@ module.exports = {
   webpack: (config, env) => {
     config.resolve.alias = Object.assign({}, config.resolve.alias, {
       'assets': path.resolve(__dirname, './src/assets'),
+  jest: (config) => {
+    config.moduleDirectories = [
+      'node_modules',
+      'src',
+    ];
+    config.moduleNameMapper = Object.assign({}, config.moduleNameMapper, {
+      '^.+\\.(|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/__mocks__/fileMock.js',
     });
+    config.coverageReporters = [
+      'json',
+      'html',
+      'text',
+    ];
     return config;
   },
 };
